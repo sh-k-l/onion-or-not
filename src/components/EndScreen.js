bf11c28a27f066b05623c23ea72421cbfb0ae1dd
Shakil <shakil.chowdhury@yahoo.com>
2019-03-12 21:06:26 +0000

    v centre stuff, changed info

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Cookies from 'universal-cookie';
-// import "./EndScreen.scss";
+import "./EndScreen.scss";
 
 function EndScreen(props){
     const cookies = new Cookies();
--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -10,10 +10,10 @@ function SplashScreen(props){
                 <p>Your high score: {props.topScore}</p>
                 <button onClick={props.click}>GO!</button>
                 <div className="info">
-
                     <p>How to play:</p>
-                    <p>The game gets a random post from <a href="www.reddit.com/r/nottheonion">/r/nottheonion</a> and a random post from <a href="www.reddit.com/r/theonion">/r/theonion</a>.</p>
+                    <p>The game gets two random headlines, one from <a href="www.reddit.com/r/nottheonion">/r/nottheonion</a> and one from <a href="www.reddit.com/r/theonion">/r/theonion</a>.</p>
                     <p>You need to decide which is the real headline (the post from <a href="www.reddit.com/r/nottheonion">/r/nottheonion</a>)</p>
+                    <p>How many can you get in a row?</p>
                 </div>
             </div>
         </div>
