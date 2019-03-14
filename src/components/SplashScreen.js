d25a1172907f1d30436fb3333a6c9ad794419c0e
sh-k-l <shakil.chowdhury@yahoo.com>
2019-03-14 16:13:31 +0000

    Tidy game info

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -12,8 +12,8 @@ function SplashScreen(props){
                 <div className="info">
                     <p>How to play:</p>
                     <ul>
-                        <li>The game gets two random headlines, one from <a href="http://www.reddit.com/r/nottheonion">/r/nottheonion</a> and one from <a href="www.reddit.com/r/theonion">/r/theonion</a>.</li>
-                        <li>You need to decide which is the real headline (the post from <a href="http://www.reddit.com/r/nottheonion">/r/nottheonion</a>).</li>
+                        <li>The game gets two random headlines, one from <a href="http://www.reddit.com/r/nottheonion">/r/nottheonion</a> (real headlines) and one from <a href="www.reddit.com/r/theonion">/r/theonion</a> (fake headlines).</li>
+                        <li>You need to click on the real headline (the post from <a href="http://www.reddit.com/r/nottheonion">/r/nottheonion</a>).</li>
                         <li>How many can you get in a row?</li>
                     </ul>
                 </div>
