fb6182263c8a64df19d40314dc4f3aedc6e65aa4
sh-k-l <shakil.chowdhury@yahoo.com>
2019-03-22 14:59:06 +0000

    updated end scrnee

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -6,21 +6,30 @@ import Review from "./Review";
 const EndScreen = (props) => {
     const cookies = new Cookies();
     let topScore = cookies.get("oonTopScore") || 0;
+
+    let body = [
+        <p class="score">Your score: {props.score}</p>,
+        <p>Your high score: {topScore}</p>
+    ];
+
     if(topScore < props.score){
         topScore = props.score;
         cookies.set("oonTopScore", topScore, { path: '/' });
+        body = [
+            <p>NEW HIGH SCORE</p>,
+            <p className="high-score">You scored: {props.score}!</p>            
+        ]
     }
 
     return(
         <div className="end-screen">
-            {/* <div className=""> */}
-              <div className="gg">
-                <h2>GG!</h2>
-                <p>Your score: {props.score}</p>
-                <p>Your high score: {topScore}</p>
-                <button onClick={props.click}>Try again</button>
-              </div>
-            {/* </div> */}
+
+            <div className="gg">
+            <h2>GG!</h2>
+            {body}
+            <button onClick={props.click}>Try again</button>
+            </div>
+
             <div className="review-wrapper">
                 <Review rounds={props.rounds}/>
             </div>
